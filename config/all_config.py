bc90d57b9d0f84de3315b2ca0b590da061f8e56f
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2025-04-08 23:16:49 -0700

    Update all_config.py

--- config/all_config.py
+++ config/all_config.py
@@ -12,7 +12,7 @@ class AllConfig(Config):
         
         # Basic training parameters
         parser.add_argument('--img_size', type=int, default=256)
-        parser.add_argument('--num_epochs', type=int, default=1)
+        parser.add_argument('--num_epochs', type=int, default=50)
         parser.add_argument('--batch_size', type=int, default=12)
         parser.add_argument('--learning_rate', type=float, default=1e-3)
 
